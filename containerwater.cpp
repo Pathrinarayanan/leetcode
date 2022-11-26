90b5d8c5ff383a1d7d5bde14512e66b9d5cc507c
PATHRI NARAYANAN <74031721+Pathrinarayanan@users.noreply.github.com>
2022-11-26 12:57:35 +0530

    Create containerwater.cpp

---
+++ containerwater.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left =0 ;int right = height.size()-1;
+        int area = INT_MIN;
+        while(left<right){
+            if(height[left] < height[right]){
+                area = max(area, height[left] * (right-left));
+                left++;
+            }
+            else{
+                area = max(area, height[right] * (right-left));
+                right--;
+            }
+        }
+        return area;
+    }
+};
